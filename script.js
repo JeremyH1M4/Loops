e6eec184e2c0d6c02ab409cda6b570e181042b1a
jeremhuy28 <jeremhuy28@g.crookcountyschools.org>
2025-10-16 15:27:13 -0700

    Assignment Completed

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict'
+console.log("hello world");
+
+for(let i = 4; i <= 1000000; i = i*1.10){
+    console.log(i);
+};
+
+const M1= ["M1", "M2", "M3", "M4", "M5", "M6"];
+let i=0
+while(M1[i]) {
+    console.log(M1[i])
+    i++
+};
+
+
+for(let j = 1; j <= 100; j = j+2){
+    console.log(j);
+};
+
+const PT= ["Happiness", "Sadness", "Anger", "Shame", "Regret", "Envy"];
+let j=0
+while(PT[j]) {
+    document.write(PT[j])
+    console.log(PT[j])
+    j++
+};
